375b0ba895c1caaaad48ceee79ed005e2069c642
MaryMaryVery <kalchenko.marya@gmail.com>
2022-02-24 20:01:56 +0300

    0.1.0

--- script.js
+++ script.js
@@ -6,17 +6,26 @@ const playSound = (e) => {
   button.classList.add("transform");
   audio.currentTime = 0;
   audio.play();
-
 };
 
 window.addEventListener("keydown", playSound);
 
 const removeTransition = (e) => {
-  if (e.propertyName !== "transform")return;
-     e.target.classList.remove("transform");
-     console.log(e.propertyName);
+  if (e.propertyName !== "transform") return;
+  e.target.classList.remove("transform");
 };
 
 const keys = document.querySelectorAll("button");
 
 keys.forEach((el) => el.addEventListener("transitionend", removeTransition));
+
+const playClickSound = (e) => {
+  const audio = document.querySelector(`audio[data-key="${e.target.getAttribute("data-key")}"]`);
+
+  if (!audio) return;
+  e.target.classList.add("transform");
+  audio.currentTime = 0;
+  audio.play();
+};
+
+keys.forEach((el) => el.addEventListener("click", playClickSound));
